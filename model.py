bdb585e0d5008c92ace22e159a643513d15ed384
Darius Petermann <darius.petermann@gmail.com>
2020-12-29 17:29:59 +0000

    Finalizing waveunet

--- model.py
+++ model.py
@@ -3,85 +3,133 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
+class Upsample(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        x = torch.unsqueeze(x, 1)
+        x = F.upsample(x, size=(1, x.get_shape().as_list()[2]*2), mode='bilinear')
+        x = torch.squeeze(x, 1)
+        return x
+
+class Downsample(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return x[:,::2,:]
+
 class Waveunet(nn.Module):
-    def __init__(
-        self,
+    def __init__(self,
         n_frames = 16384,
-        num_layers = 12, # How many U-Net layers
-        filter_size = 15, # Filter size of conv in downsampling block
+        num_layers = 12,
+        filter_size = 15,
         num_filters = 24,
         kernel_size_down = 15,
         kernel_size_up = 5,
-        output_filter_size = 1, # Filter size of convolution in the output layer
+        output_filter_size = 1,
         output_activation  = 'tanh',
         strides = 1
     ):
-        """
-        Input: (nb_samples, nb_in_channels)
-        Output: (nb_samples, nb_out_channels)
-        """
 
-        super(Waveunet, self).__init__()
+        self.num_layers   = num_layers
+        self.enc_conv     = []
+        self.dec_conv     = []
+        self.skip         = []
+        self.dec_num_filt = []
+        self.out_frames   = n_frames
 
-        x = inputs
+        self.leaky = nn.LeakyReLU(alpha=0.2)
+        self.bn1   = BatchNorm1d()
+        self.us    = Upsample()
+        self.ds    = Downsample()
+        self.out_conv = self.dec_conv(nn.Conv1D(filters=1,kernel_size=1))
 
         # Encoding Path
         for layer in range(num_layers):
 
             num_filters_layer = num_filters + (num_filters * layer)
-            out_frames = out_frames / 2
 
-            x = nn.Conv1D(
-                in_channels=self.n_frames,
+            self.enc_conv.append(nn.Conv1D(
+                in_channels=n_frames,
                 out_channels=out_frames,
                 filters=num_filters_layer, 
-                kernel_size=self.kernel_size_down,
-                padding=(self.kernel_size_down // 2),
-                strides=self.strides)(x)
+                kernel_size=kernel_size_down,
+                padding=(kernel_size_down // 2),
+                strides=strides))
+            
+            self.dec_num_filt.append(num_filters_layer)
 
-            x = LeakyReLU(alpha=0.2)(x)
-            x = BatchNorm1d()(x)
+            out_frames = out_frames / 2
 
-            # Save skip connection for decoding path and downsample
-            skip_connections.append(x)
-            x = Lambda(downsample)(x)
-
-        x = Conv1D(
+        self.conv_bottleneck = Conv1D(
+            out_channels=out_frames,
             filters=num_filters + (num_filters * num_layers), 
             kernel_size=kernel_size_down,
-            padding='same',
+            padding=(kernel_size_down // 2),
             strides=strides)(x)
 
         # Decoding Path
         for layer in range(num_layers):
 
-            skip_layer = skip_connections[-layer-1]
+            num_filters_layer = self.dec_num_filt[-layer-1]
+            out_frames = out_frames * 2
+
+            self.dec_conv(nn.Conv1D(
+                out_channels=out_frames,
+                filters=num_filters_layer, 
+                kernel_size=kernel_size_up,
+                padding=(kernel_size_down // 2),
+                strides=strides))
+
+
+    super(Waveunet, self).__init__()
+
+    def forward(self,x):
+        """
+        Input: (nb_samples, nb_in_channels)
+        Output: (nb_samples, nb_out_channels)
+        """
+
+        inputs = x
+
+        # Encoding Path
+        for layer in range(self.num_layers):
+
+            num_filters_layer = num_filters + (num_filters * layer)
+            out_frames = out_frames / 2
+
+            x = self.enc_conv[layer](x)
+
+            x = self.leaky(x)
+            x = self.bn1(x)
+
+            # Save skip connection for decoding path and downsample
+            self.skip.append(x)
+            x = self.downsample(x)
+
+        x = self.conv_bottleneck(x)
+
+        # Decoding Path
+        for layer in range(num_layers):
+
+            skip_layer = self.skip[-layer-1]
             
-            # Makesure that num_filter coincide with current layer shape
+            # Make sure that num_filter coincide with current layer shape
             num_filter = skip_layer.get_shape().as_list()[2]
 
             # Upsample and Concatenate
-            x = Lambda(upsample)(x)
-            x = Concatenate(axis=2)([x, skip_layer])
+            x = self.us(x)
+            x = torch.cat((x, skip_layer),2)
+            x = self.dec_conv[layer](x)
 
-            x = Conv1D(
-                filters=num_filter, 
-                kernel_size=kernel_size_up,
-                padding='same',
-                strides=strides)(x)
-
-            x = LeakyReLU(alpha=0.2)(x)
-            x = BatchNormalization()(x)
+            x = self.leaky(x)
+            x = self.bn1(x)
 
         # Last concat is with original input
-        x = Concatenate(axis=2)([inputs, x])
+        x = torch.cat((x, inputs),2)
 
         # Collapse feature maps into N sources
-        out_bass   = Conv1D(filters=1, kernel_size=1, padding='same',activation='tanh')(x)
-        out_drums  = Conv1D(filters=1, kernel_size=1, padding='same',activation='tanh')(x)
-        out_other  = Conv1D(filters=1, kernel_size=1, padding='same',activation='tanh')(x)
-        out_vocals = Conv1D(filters=1, kernel_size=1, padding='same',activation='tanh')(x)
-
-        outputs = Concatenate(axis=-1)([out_bass,out_drums])
-        outputs = Concatenate(axis=-1)([outputs,out_other])
-        outputs = Concatenate(axis=-1)([outputs,out_vocals])
+        y   = torch.tanh(self.out_conv(x))
