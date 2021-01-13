8732ce74a6d61962e9c5c8bdfa21dba856c398b9
Darius Arthur Petermann <daripete@gan.luddy.indiana.edu>
2021-01-12 23:17:19 -0500

    Added new pipeline

--- train.py
+++ train.py
@@ -3,6 +3,8 @@ import torch
 import torch.nn as nn
 import numpy as np
 import random
+from pathlib import Path
+
 import torch.nn.functional as F
 from torchvision import datasets, transforms
 from torch.autograd import Variable
@@ -21,7 +23,71 @@ import IPython.display as ipd
 import soundfile as sf
 import logging
 
+import argparse
 import model
+import data
+
+parser = argparse.ArgumentParser(description='Open Unmix Trainer')
+
+# which target do we want to train?
+parser.add_argument('--target', type=str, default='vocals',
+                    help='target source (will be passed to the dataset)')
+
+# Dataset paramaters
+parser.add_argument('--dataset', type=str, default="musdb",
+                    choices=[
+                        'musdb', 'aligned', 'sourcefolder',
+                        'trackfolder_var', 'trackfolder_fix'
+                    ],
+                    help='Name of the dataset.')
+parser.add_argument('--root', type=str, help='root path of dataset')
+parser.add_argument('--output', type=str, default="open-unmix",
+                    help='provide output path base folder name')
+parser.add_argument('--model', type=str, help='Path to checkpoint folder')
+
+# Trainig Parameters
+parser.add_argument('--epochs', type=int, default=1000)
+parser.add_argument('--batch-size', type=int, default=16)
+parser.add_argument('--lr', type=float, default=0.001,
+                    help='learning rate, defaults to 1e-3')
+parser.add_argument('--patience', type=int, default=140,
+                    help='maximum number of epochs to train (default: 140)')
+parser.add_argument('--lr-decay-patience', type=int, default=80,
+                    help='lr decay patience for plateau scheduler')
+parser.add_argument('--lr-decay-gamma', type=float, default=0.3,
+                    help='gamma of learning rate scheduler decay')
+parser.add_argument('--weight-decay', type=float, default=0.00001,
+                    help='weight decay')
+parser.add_argument('--seed', type=int, default=42, metavar='S',
+                    help='random seed (default: 42)')
+
+# Model Parameters
+parser.add_argument('--seq-dur', type=float, default=6.0,
+                    help='Sequence duration in seconds'
+                    'value of <=0.0 will use full/variable length')
+parser.add_argument('--unidirectional', action='store_true', default=False,
+                    help='Use unidirectional LSTM instead of bidirectional')
+parser.add_argument('--nfft', type=int, default=4096,
+                    help='STFT fft size and window size')
+parser.add_argument('--nhop', type=int, default=1024,
+                    help='STFT hop size')
+parser.add_argument('--hidden-size', type=int, default=512,
+                    help='hidden size parameter of dense bottleneck layers')
+parser.add_argument('--bandwidth', type=int, default=16000,
+                    help='maximum model bandwidth in herz')
+parser.add_argument('--nb-channels', type=int, default=2,
+                    help='set number of channels for model (1, 2)')
+parser.add_argument('--nb-workers', type=int, default=0,
+                    help='Number of workers for dataloader.')
+
+# Misc Parameters
+parser.add_argument('--quiet', action='store_true', default=False,
+                    help='less verbose during training')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+
+args, _ = parser.parse_known_args()
+print(args.nb_channels)
 
 device = torch.device("cuda")
 
@@ -31,13 +97,6 @@ allPaths=[]
 allPaths+= [os.path.join(rootPath,song) for song in os.listdir(rootPath) if not os.path.isdir(os.path.join(rootPath, song))]
 totLen=len(allPaths)
 random.seed(0)
-
-brokens = ["0108-00003-HMD.wav", "0063-00002-HMD.wav", "0150-00003-RMD.wav", "0122-00003-HMD.wav", "0003-00001-AMD.wav"]
-ii = []
-for p in range(0, totLen):
-    if os.path.basename(allPaths[p]) in brokens:
-        ii.append(p)
-allPaths = np.delete(allPaths,ii)
 
 random.shuffle(allPaths)
 trPaths=allPaths[:np.int(totLen*.9)]
@@ -50,60 +109,132 @@ maxEpoch=300
 BS=20
 minLen=16384
 
-m = model.Waveunet().to(device)
-#summary(m,(1,16384),device='cpu')
-
-optimizer= torch.optim.Adam(m.parameters(), lr=1e-3)
 maxValSNR=-1000
 scale=1e-10
 zeropadding=1024+7040
 
-m.train()
 random.shuffle(trPaths)
 tic=time.time()
 
-with torch.cuda.device(device):
+def train(args, m, device, train_sampler, optimizer):
+    losses = utils.AverageMeter()
+    unmix.train()
+    pbar = tqdm.tqdm(train_sampler, disable=args.quiet)
+    for x, y in pbar:
+        pbar.set_description("Training batch")
+        x, y = x.to(device), y.to(device)
+        optimizer.zero_grad()
+        Y_hat = unmix(x)
+        Y = unmix.transform(y)
+        loss = torch.nn.functional.mse_loss(Y_hat, Y)
+        loss.backward()
+        optimizer.step()
+        losses.update(loss.item(), Y.size(1))
+    return losses.avg
 
-    for epoch in range(maxEpoch):
+use_cuda = torch.cuda.is_available()
+print("Using GPU:", use_cuda)
+dataloader_kwargs = {'num_workers': args.nb_workers, 'pin_memory': True} if use_cuda else {}
 
-        m.train()
-        random.shuffle(trPaths)
-        tic=time.time()
-        for p in range(0, len(trPaths), BS):
+repo_dir = os.path.abspath(os.path.dirname(__file__))
 
-            if p+BS>len(trPaths):
-                BStail=len(trPaths)-p
-            else:
-                BStail=BS
-                
-            x=torch.zeros((BStail, 2, minLen), device=device)
-            
-            for bs in range(BStail):
-                filename=trPaths[p+bs]
-                waveform, sr = torchaudio.load(filename)
-                waveform=torchaudio.transforms.Resample(sr, 44100)(waveform)[:,:minLen]
-    #             x_temp, _=librosa.load(p, sr=None, mono=False)
-                x[bs,]=waveform.to(device)
-            mb = torch.mean(x, axis=1, keepdim=True)
-    #         mbl=x[:,0,None,:]
-    #         mbr=x[:,1,None,:]
-    #         print(mb.size(), mbl.size(), mbr.size())
-            y = m(torch.cat((torch.randn((BStail, 1, zeropadding))*scale, mb), 2)) #896 for W=32
-            y = y.to(device)
-            mb = mb.to(device)
-    #         print(yl.shape[2]-mbl.shape[2])
-    #         err=(-SNR(mbl.squeeze(), yl.squeeze())-SNR(mbr.squeeze(), yr.squeeze()))/2
-            err=torch.mean((mb-y)**2)
-            optimizer.zero_grad()
-            err.backward()
-            optimizer.step()                                    
-            
-            # if p%20 == 0 and p is not 0:
-            #     with torch.no_grad():
-            #         valSNR=FCN_AE_test(m, vPaths, scale, zeropadding, quantization=False, writeout=False)
-            #     if valSNR > maxValSNR:
-            #         maxValSNR = valSNR
-            #         torch.save(model.state_dict(), 'FCN_AE_Music_LV{}.model'.format(Hc)) 
-            #     toc=time.time()
-            #     logging.info('ep: {0}\t err: {1:3.4}\t SNR: {2:3.4}\t max SNR: {3:3.4}\t time: {4:3.3}'.format(epoch, err.detach().cpu(), valSNR, maxValSNR, toc-tic))    
-            #     tic=time.time()
+# use jpg or npy
+torch.manual_seed(args.seed)
+random.seed(args.seed)
+
+device = torch.device("cuda" if use_cuda else "cpu")
+
+train_dataset, valid_dataset, args = data.load_datasets(parser, args)
+
+# create output dir if not exist
+target_path = Path("./output")
+target_path.mkdir(parents=True, exist_ok=True)
+
+# train_sampler = torch.utils.data.DataLoader(
+#     train_dataset, batch_size=BS, shuffle=True,
+#     **dataloader_kwargs
+# )
+# valid_sampler = torch.utils.data.DataLoader(
+#     valid_dataset, batch_size=1,
+#     **dataloader_kwargs
+# )
+
+# waveunet = model.Waveunet().to(device)
+# #summary(m,(1,16384),device='cpu')
+
+# optimizer = torch.optim.Adam(
+#     unmix.parameters(),
+#     lr=args.lr,
+#     weight_decay=args.weight_decay
+# )
+
+# scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+#     optimizer,
+#     factor=args.lr_decay_gamma,
+#     patience=args.lr_decay_patience,
+#     cooldown=10
+# )
+
+# es = utils.EarlyStopping(patience=args.patience)
+
+# t = tqdm.trange(1, args.epochs + 1, disable=args.quiet)
+# train_losses = []
+# valid_losses = []
+# train_times = []
+# best_epoch = 0
+
+# for epoch in t:
+#     t.set_description("Training Epoch")
+#     end = time.time()
+#     train_loss = train(args, waveunet, device, train_sampler, optimizer)
+#     valid_loss = valid(args, waveunet, device, valid_sampler)
+#     scheduler.step(valid_loss)
+#     train_losses.append(train_loss)
+#     valid_losses.append(valid_loss)
+
+#     t.set_postfix(
+#         train_loss=train_loss, val_loss=valid_loss
+#     )
+
+#     stop = es.step(valid_loss)
+
+#     if valid_loss == es.best:
+#         best_epoch = epoch
+
+#     utils.save_checkpoint({
+#             'epoch': epoch + 1,
+#             'state_dict': waveunet.state_dict(),
+#             'best_loss': es.best,
+#             'optimizer': optimizer.state_dict(),
+#             'scheduler': scheduler.state_dict()
+#         },
+#         is_best=valid_loss == es.best,
+#         path=target_path,
+#         target=args.target
+#     )
+
+#     # save params
+#     params = {
+#         'epochs_trained': epoch,
+#         'args': vars(args),
+#         'best_loss': es.best,
+#         'best_epoch': best_epoch,
+#         'train_loss_history': train_losses,
+#         'valid_loss_history': valid_losses,
+#         'train_time_history': train_times,
+#         'num_bad_epochs': es.num_bad_epochs,
+#         'commit': commit
+#     }
+
+#     with open(Path(target_path,  args.target + '.json'), 'w') as outfile:
+#         outfile.write(json.dumps(params, indent=4, sort_keys=True))
+
+#     train_times.append(time.time() - end)
+
+#     if stop:
+#         print("Apply Early Stopping")
+#         break
+
+
+# if __name__ == "__main__":
+# main()
